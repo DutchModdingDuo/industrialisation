a9d2b94df5f88ff6a9382387e530cdff21e50b47
bonka95 <github@merlijnvermeer.nl>
2015-03-31 16:29:15 +0200

    added mcmod.info

--- src/main/java/com/bonka95/industrialisation/industrialisation.java
+++ src/main/java/com/bonka95/industrialisation/industrialisation.java
@@ -1,11 +1,35 @@
 package com.bonka95.industrialisation;
 
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid="Industrialisation", name="Industrialisation", version="1.7.10-1.0B")
 
 /**
  * Created by Merlijn on 31-3-2015.
  */
-public class industrialisation {
+public class Industrialisation
+{
+    @Mod.Instance("Industrialisation")
+    public static Industrialisation instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+
+    }
 }
