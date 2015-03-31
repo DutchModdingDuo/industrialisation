24522e131346053617531df3818544c221fd345c
bonka95 <github@merlijnvermeer.nl>
2015-03-31 15:42:03 +0200

    added mod start

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # industrialisation
-industrialisation mod for minecraft
+Industrialisation mod for minecraft.
+This is the first mod I made to get started.
+And to begin to understand how you can make a mod in minecraft.
+
+I hope you like it :D
---
+++ src/main/java/com/bonka95/industrialisation/industrialisation.java
@@ -0,0 +1,11 @@
+package com.bonka95.industrialisation;
+
+import cpw.mods.fml.common.Mod;
+
+@Mod(modid="Industrialisation", name="Industrialisation", version="1.7.10-1.0B")
+
+/**
+ * Created by Merlijn on 31-3-2015.
+ */
+public class industrialisation {
+}
